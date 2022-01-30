264c65ea07f27de059ba35a240930107d6cb0695
Jacob <57625094+Snake0good@users.noreply.github.com>
2022-01-30 16:03:18 +0800

    update projects and scrolling

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-const logo = document.getElementById('logo')
-const topNav = document.getElementById('myTopNav')
+/*
+var dropdown = document.getElementById("hamburger-dropdown");
+var menu = document.getElementById("menu")
+var close = document.getElementById("close")
 
-window.onscroll = function() {
-  topNav.style.height='0'
-  logo.style.opacity = '0'
+function hideDropdown() {
+  if (dropdown.style.display === 'block') {
+    dropdown.style.display = "none";
+    menu.style.visibility = "visible"
+    close.style.visibility = "hidden"
+  } else {
+    dropdown.style.display = "block";
+    menu.style.visibility = "hidden"
+    close.style.visibility = "visible"
+  }
 }
 
+*/
+
+const topNav = document.getElementById('myTopNav')
+const navBar = document.getElementById("nav-bar")
+
+window.onscroll = function() {
+  topNav.style.height='0';
+  navBar.style.opacity = '0';
+}
+
+window.onclick = function() {
+  navBar.style.opacity = '1';
+}
 
 function openNav() {
-  topNav.style.height = "200px"
-  logo.style.opacity = '0'
+  topNav.style.height = "200px";
 }
 
 function closeNav() {
   topNav.style.height = "0";
-  logo.style.opacity = '1'
 }
